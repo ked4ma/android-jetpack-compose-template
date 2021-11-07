ba63cf6690cb2e849e7ca8c6b8bf0cabbfc0b919
ked4ma <hades.netherworld@gmail.com>
2021-11-07 19:26:15 +0900

    use version catalog for dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath(libs.android.build.gradle)
+        classpath(libs.kotlin.gradle)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,10 @@
 dependencyResolutionManagement {
+    // enable feature preview
+    // ref. https://docs.gradle.org/7.0.2/userguide/platforms.html#sub:central-declaration-of-dependencies
+    enableFeaturePreview("VERSION_CATALOGS")
+    // default name is "libs", it only shows to set value here. (yes, it's a redundant line)
+    defaultLibrariesExtensionName.set("libs")
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
